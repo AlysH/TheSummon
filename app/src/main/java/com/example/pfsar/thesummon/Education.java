d13503cd419f98ad26f7705b1ac77740ad0681d1
Sophie Ashcroft <ms.sophie.ashcroft@gmail.com>
2018-10-20 23:04:10 +0200

    adding education activity and basic logic, updating values with string arrays for education institutions and education levels, starting to build out a fundimental style

--- app/src/main/java/com/example/pfsar/thesummon/Education.java
+++ app/src/main/java/com/example/pfsar/thesummon/Education.java
@@ -1,13 +1,21 @@
 package com.example.pfsar.thesummon;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Education extends AppCompatActivity {
+
+    public Education() {
+        educationLevelArray = res.getStringArray(R.array.education_level_array);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_education);
     }
+
+    Resources res = getResources();
+    String[] educationLevelArray;
 }
