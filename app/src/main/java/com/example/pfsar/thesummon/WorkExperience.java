63b6c307fd9519300aaa3824872c9530a6a12e8a
Sophie Ashcroft <ms.sophie.ashcroft@gmail.com>
2018-10-21 02:28:50 +0200

    adding layout title styles and applying it to work experience, also defining constraints withing work experience

--- app/src/main/java/com/example/pfsar/thesummon/WorkExperience.java
+++ app/src/main/java/com/example/pfsar/thesummon/WorkExperience.java
@@ -1,11 +1,7 @@
 package com.example.pfsar.thesummon;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
 public class WorkExperience extends AppCompatActivity {
 
@@ -13,9 +9,5 @@ public class WorkExperience extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_experience);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
     }
-
 }
